ecf4cda379dd251141f52255bc8195facab1c9ac
yuslove1 <adexyuslove@gmail.com>
2024-10-11 03:34:47 -0700

    feat: setup routes

---
+++ MovieDB-app/src/App.jsx
@@ -0,0 +1,47 @@
+
+import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Layout from "./components/common/Layout";
+import HeroPage from "./components/Pages/HeroPage";
+import SearchResult from "./components/Pages/SearchResult";
+import ViewDetails from "./components/Pages/ViewDetails";
+import NotFound from "./components/Pages/NotFound";
+import PageLayout from "./components/common/PageLayout";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageLayout />,
+      children: [
+        {
+          path: "/",
+          index:true,
+          element: <HeroPage />,
+        }, 
+        {
+          path: "search-result",
+          element: <SearchResult />,
+        }, 
+        {
+          path: "detail/:movieId",
+          element: <ViewDetails />,
+        }, 
+        {
+          path: "*",
+          element: <NotFound />,
+        }, 
+      ]
+    }
+  ]);
+
+
+function App() {
+   return <RouterProvider router={router} />
+}
+export default App;
