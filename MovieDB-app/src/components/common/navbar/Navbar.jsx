f8ffcf6f1d3881aac994c1e3a30a44eab12a8788
yuslove1 <adexyuslove@gmail.com>
2024-10-16 01:40:11 -0700

    fix: corrected a sytax error

--- MovieDB-app/src/components/common/navbar/Navbar.jsx
+++ MovieDB-app/src/components/common/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../logo";
+import Logo from "../Logo";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 import MobileNav from "./MobileNav";
