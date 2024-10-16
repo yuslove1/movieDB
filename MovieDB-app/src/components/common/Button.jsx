23a3ba3fc8b0863c153ad5d40dacb521aa1964b6
yuslove1 <adexyuslove@gmail.com>
2024-10-16 01:22:07 -0700

    style : improved the button styling

--- MovieDB-app/src/components/common/Button.jsx
+++ MovieDB-app/src/components/common/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Button = (props) => {
-const {text, isRound, width, handleClick,} = props;
+const {text, isRound, width, handleClick} = props;
   return (
-    <div className="max-w-[80%] mx-auto flex justify-center">
-      <button className={`bg-darkcyan p-4 text-white ${isRound ? 'rounded-lg' : ''} ${width}`} onClick={handleClick}>{text}</button>
+    <div className="max-w-[50%] mx-auto flex justify-center py-2">
+      <button className={`transition-all  bg-darkcyan hover:bg-transparent hover:border-2 hover:border-darkcyan hover:text-darkcyan font-bold p-4 text-white ${isRound ? 'rounded-full' : ''} ${width}`} onClick={handleClick}>{text}</button>
     </div>
   )
 }
 
-export default Button
+export default Button;
